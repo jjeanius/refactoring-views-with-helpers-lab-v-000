5005cc051436097050575f689de7f205c33144ff
Judy McGoldrick <jjeanius@verizon.net>
2019-02-09 05:10:47 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,9 +6,10 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
+    song = Song.create(title: title)
+
   #  artist? self.artist.display_artist.name:nil
     self.artist = Artist.find_or_create_by(name: name)
-    song = Song.create(title: title)
     song.artist.name = artist_name
 
   end
