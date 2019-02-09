c45d6af929a3fa7d249c543e9bab09c571e2172e
Judy McGoldrick <jjeanius@verizon.net>
2019-02-09 04:51:01 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,7 +8,7 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
   #  artist? self.artist.display_artist.name:nil
     song = Song.create(title: title)
-    song.artist_name = artist_name
+    song.artist.name = artist_name
 
   end
 end
