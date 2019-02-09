31e28fce0d35f094f7a1ba1d3c65246590d70e1e
Judy McGoldrick <jjeanius@verizon.net>
2019-02-09 04:40:07 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -7,7 +7,7 @@ module ArtistsHelper
   
       link_to song.artist.name, artist_path(song.artist)
     else 
-      binding.pry
+    #  binding.pry
       link_to "Add Artist", edit_song_path(song)
     end
   end
